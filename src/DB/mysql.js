52ed326f6f5e7ec4f0f8d21458eccf278bb397cb
Lugaldz <kink-2@hotmail.com>
2023-03-23 21:46:07 -0600

    primer commit

---
+++ src/DB/mysql.js
@@ -0,0 +1,97 @@
+const mysql = require('mysql');
+const config = require('../config');
+
+const dbconfig = {
+    host: config.mysql.host,
+    user: config.mysql.user,
+    password: config.mysql.password,
+    database: config.mysql.database,
+}
+
+let conexion;
+
+function conMysql(){
+    conexion = mysql.createConnection(dbconfig);
+
+    conexion.connect((err)=>{
+        if(err){
+            console.log('[db err]',err);
+            setTimeout(conMysql,200);
+        }else{
+            console.log('DB conectada')
+        }
+    });
+    conexion.on('error',err=>{
+        console.log('[db err]',err);
+        if(err.code ==='PROTOCOL_CONNECTION_LOST'){
+            conMysql();
+        }else{
+            throw err;
+        }
+
+    })
+}
+
+conMysql();
+
+function todos(tabla){
+    return  new Promise((resolve, reject)=>{
+        conexion.query(`SELECT * FROM ${tabla}`, (error,result)=>{
+            return error ? reject(error) : resolve(result);
+        })
+    });
+
+}
+
+function uno(tabla, id){
+    return  new Promise((resolve, reject)=>{
+        conexion.query(`SELECT * FROM ${tabla} WHERE id${tabla}=${id}`, (error,result)=>{
+            return error ? reject(error) : resolve(result);
+        })
+    });
+
+}
+
+function insertar(tabla, data){
+    return  new Promise((resolve, reject)=>{
+        conexion.query(`INSERT INTO ${tabla} SET ?`,data, (error,result)=>{
+            return error ? reject(error) : resolve(result);
+        })
+    });
+
+}
+
+function actualizar(tabla, data){
+    let id = Object.values(data);
+    console.log("el id es:"+id[0]);
+    return  new Promise((resolve, reject)=>{
+        conexion.query(`UPDATE ${tabla} SET ? WHERE id${tabla} = ?`,[data, id[0]], (error,result)=>{
+            return error ? reject(error) : resolve(result);
+        })
+    });
+
+}
+
+function agregar(tabla, data){
+    let id = Object.values(data);
+    if(data && id[0] == 0){
+        return insertar(tabla, data);
+    }else{
+        return actualizar(tabla, data);
+    }
+
+} 
+
+function eliminar(tabla, data){
+    let id = Object.values(data);
+    return  new Promise((resolve, reject)=>{
+        conexion.query(`DELETE FROM ${tabla} WHERE id${tabla}= ?`, id[0], (error,result)=>{
+            return error ? reject(error) : resolve(result);
+        })
+    });
+
+}
+
+module.exports= {
+    todos,uno,agregar,eliminar
+}
---
+++ src/app.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const morgan = require('morgan');
+const config = require('./config');
+const cors = require('cors');
+
+const roles = require('./modulos/roles/rutas');
+const permisos = require('./modulos/permisos/rutas');
+const permisosRoles = require('./modulos/permisosRoles/rutas');
+const error = require('./red/errors');
+
+const app = express();
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+/*app.use((req,res,next)=>{
+    res.setHeader("Access-Control-Allow-Origin","*");
+    next();
+});*/
+
+
+
+//configuracion
+app.set('port',config.app.port);
+
+const whiteList = [process.env.ORIGIN1];
+app.use(cors({
+    origin: function(origin, callback){
+        if(!origin || whiteList.includes(origin)){
+            return callback(null, origin);
+        }
+        return callback("Error de CORS origin: " + origin + " NO AUTORIZADO!")
+    },
+    //Habilitamos el uso de credenciales
+    //credentials: true
+}));
+
+//rutas
+app.use('/api/roles',roles);
+app.use('/api/permisos',permisos);
+app.use('/api/permisosRoles',permisosRoles);
+app.use(error);
+
+
+module.exports = app;
