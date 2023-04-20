0ad755b7abbabac2a5599a5ea368e44dfd0b93f2
DulceTovar01 <duulcetovar@gmail.com>
2023-04-20 13:29:14 -0600

    bajar back

--- src/config.js
+++ src/config.js
@@ -9,7 +9,7 @@ module.exports = {
 
         host: process.env.MYSQL_HOST || 'localhost',
         user: process.env.MYSQL_USER || 'root',
-        password: process.env.MYSQL_PASSWORD || '13de4del2002',
+        password: process.env.MYSQL_PASSWORD || 'its2022',
         database: process.env.MYSQL_DB || 'italika'
     }
 }
